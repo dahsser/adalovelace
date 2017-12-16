c20b7a943d0f4538d405a18ddd513eaaf62d08c1
Diego Lopez <deidago7@hotmail.com>
2017-12-16 12:34:18 -0500

    my changes

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const mustache = require('mustache-express');
 const app = express();
+
+app.use("/", express.static(__dirname + '/webapp/static'));
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/webapp/views');
+
+app.get("/", function(req, res){
+    res.render("index");
+});
